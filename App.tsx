0fb7556a677efff038c2d740314d9d9094e2d232
julian <ingsistemasjulian@gmail.com>
2022-01-06 10:27:07 -0500

    67. Height, Width porcentual y dimensiones de la pantalla

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { SafeAreaView } from 'react-native'
 import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen'
 import { ContadorScreen } from './src/screens/ContadorScreen'
+import { DimensionesScreen } from './src/screens/DimensionesScreen'
 import { HolaMundoScreen } from './src/screens/HolaMundoScreen'
 
 export const App = () => {
@@ -9,7 +10,8 @@ export const App = () => {
       
       // Para ios es necesario usar SafeAreaView para que omita el notch y se vea bien en este caso el texto
       <SafeAreaView style={{ flex: 1 }}>
-        <BoxObjectModelScreen />
+        {/* <BoxObjectModelScreen /> */}
+        <DimensionesScreen />
       </SafeAreaView>
   )
 }
--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 ## Sección 5 del curso React Native (Fernando H)
 
 ### Inicio de la sección de diseño
-### Padding, Margin, Border, Width y Height
+### 66. Padding, Margin, Border, Width y Height
+### 67. Height, Width porcentual y dimensiones de la pantalla
